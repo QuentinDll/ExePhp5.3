caca075219e1c48ba8e25839947fb40e4f562ca5
QuentinDll <quentin.delplace13@gmail.com>
2018-07-31 14:33:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 5</title>
+</head>
+<body>
+  <p>
+    <?php
+    $foot = 1;
+
+    while ($foot <= 15) {
+      echo $foot ++ ." On y arrive presque <br />";
+    }
+    ?>
+  </p>
+</body>
+</html>
